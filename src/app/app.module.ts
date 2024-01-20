4077a4279b3d7e5bb12cab11f69282f66e7aa774
Lucas Matias <lucas.matias@auditrix.com.br>
2024-01-20 01:18:52 -0300

    metodo create implementado para categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,11 @@ import { MatCardModule } from "@angular/material/card";
 import { CategoriaReadComponent } from "./components/views/categoria/categoria-read/categoria-read.component";
 import { MatTableModule } from "@angular/material/table";
 import { MatButtonModule } from "@angular/material/button";
+import { CategoriaCreateComponent } from "./components/views/categoria/categoria-create/categoria-create.component";
+import { FormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 @NgModule({
   declarations: [
@@ -28,6 +33,7 @@ import { MatButtonModule } from "@angular/material/button";
     NavComponent,
     HomeComponent,
     CategoriaReadComponent,
+    CategoriaCreateComponent,
   ],
   imports: [
     BrowserModule,
@@ -41,6 +47,10 @@ import { MatButtonModule } from "@angular/material/button";
     MatTableModule,
     HttpClientModule,
     MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -3,17 +3,32 @@ import { Observable } from "rxjs";
 import { Categoria } from "./categoria.model";
 import { HttpClient } from "@angular/common/http";
 import { enviroment } from "src/environments/environment";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Injectable({
   providedIn: "root",
 })
 export class CategoriaService {
-  constructor(private http: HttpClient) {}
+  private _snack: any;
+  constructor(private http: HttpClient, private_snack: MatSnackBar) {}
 
   baseUrl: String = enviroment.baseUrl;
 
   findAll(): Observable<Categoria[]> {
     const url = "${this.baseUrl}/categorias";
     return this.http.get<Categoria[]>(url);
+  }
+
+  create(categoria: Categoria): Observable<Categoria> {
+    const url = `${this.baseUrl}/categorias`;
+    return this.http.post<Categoria>(url, categoria);
+  }
+
+  mensagem(str: String): void {
+    this._snack.open(`${str}`, "OK", {
+      horizontalPosition: "end",
+      vertocalPosition: "top",
+      duration: 3000,
+    });
   }
 }
